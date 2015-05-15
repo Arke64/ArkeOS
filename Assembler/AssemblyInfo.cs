6d29c4caf82f67019a839e9c17de07086aa4ad96
John Brochue <johnbrochue@outlook.com>
2015-05-15 18:06:15 -0400

    Initial structure.

---
+++ Assembler/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ArkeOS Assembler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArkeOS")]
+[assembly: AssemblyCopyright("Copyright © John Brochue 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e9776c8d-e3ad-4bca-aa37-0c4883463480")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
