14a33c3e8f7f6811e5dbaf6bd4f195552733e6a4
John Brochue <johnbrochue@outlook.com>
2015-05-16 22:23:54 -0400

    Added reading/writing from different location types and set the flags registers.

--- Interpreter/Interpreter.cs
+++ Interpreter/Interpreter.cs
@@ -9,6 +9,7 @@ namespace ArkeOS.Interpreter {
 		private Image image;
 		private MemoryManager memory;
 		private Dictionary<Register, ulong> registers;
+		private InstructionSize currentSize;
 
 		public Interpreter() {
 			this.image = new Image();
@@ -17,7 +18,8 @@ namespace ArkeOS.Interpreter {
 
 			this.registers = Enum.GetNames(typeof(Register)).Select(n => (Register)Enum.Parse(typeof(Register), n)).ToDictionary(e => e, e => 0UL);
 
-			this.registers[Register.R0] = 1;
+			this.registers[Register.RO] = 0;
+			this.registers[Register.RF] = ulong.MaxValue;
 		}
 
 		public void Parse(byte[] data) {
@@ -40,6 +42,8 @@ namespace ArkeOS.Interpreter {
 
 				var instruction = new Instruction(this.memory.Reader);
 
+				this.currentSize = instruction.Size;
+
 				if (instruction.Code == InstructionDefinition.Hlt.Code) {
 					return;
 				}
@@ -47,7 +51,19 @@ namespace ArkeOS.Interpreter {
 					
 				}
 				else if (instruction.Code == InstructionDefinition.Add.Code) {
-					this.Access(instruction.A, instruction.B, instruction.C, (a, b) => a + b);
+					var aa = this.GetValue(instruction.A);
+					var bb = this.GetValue(instruction.B);
+
+					if (this.currentSize == InstructionSize.EightByte && ulong.MaxValue - aa < bb) {
+						unchecked {
+							this.SetValue(instruction.C, aa + bb);
+						}
+
+						this.registers[Register.RC] = ulong.MaxValue;
+					}
+					else {
+						this.Access(instruction.A, instruction.B, instruction.C, (a, b) => a + b);
+					}
 				}
 				else if (instruction.Code == InstructionDefinition.Jiz.Code) {
 
@@ -60,23 +76,66 @@ namespace ArkeOS.Interpreter {
 			}
 		}
 
-		private ulong GetValue(Parameter p) {
-			switch (p.Type) {
-				case ParameterType.Literal: return p.Literal;
-				case ParameterType.Register: return this.registers[p.Register];
-				case ParameterType.LiteralAddress: return this.memory.ReadU64(p.Literal);
-				case ParameterType.RegisterAddress: return this.memory.ReadU64(this.registers[p.Register]);
-				default: throw new ArgumentException(nameof(p));
+		private ulong GetValue(Parameter parameter) {
+			var value = 0UL;
+
+			switch (parameter.Type) {
+				case ParameterType.Literal: value = parameter.Literal; break;
+				case ParameterType.Register: value = this.registers[parameter.Register]; break;
+				case ParameterType.LiteralAddress: value = this.memory.ReadU64(parameter.Literal); break;
+				case ParameterType.RegisterAddress: value = this.memory.ReadU64(this.registers[parameter.Register]); break;
+				default: throw new ArgumentException(nameof(parameter));
 			}
+
+			switch (this.currentSize) {
+				case InstructionSize.OneByte: value = (byte)value; break;
+				case InstructionSize.TwoByte: value = (ushort)value; break;
+				case InstructionSize.FourByte: value = (uint)value; break;
+			}
+
+			return value;
 		}
 
-		private void SetValue(Parameter p, ulong value) {
-			switch (p.Type) {
+		private void SetValue(Parameter parameter, ulong value) {
+			this.registers[Register.RZ] = value == 0 ? ulong.MaxValue : 0;
+			this.registers[Register.RS] = (value & (ulong)(1 << (2 ^ ((byte)this.currentSize * 8 - 1)))) != 0 ? ulong.MaxValue : 0;
+			this.registers[Register.RC] = 0;
+
+			switch (this.currentSize) {
+				case InstructionSize.OneByte:
+					if (value > byte.MaxValue) {
+						value &= 0xFF;
+
+						this.registers[Register.RC] = ulong.MaxValue;
+					}
+
+					break;
+
+				case InstructionSize.TwoByte:
+					if (value > ushort.MaxValue) {
+						value &= 0xFFFF;
+
+						this.registers[Register.RC] = ulong.MaxValue;
+					}
+
+					break;
+
+				case InstructionSize.FourByte:
+					if (value > uint.MaxValue) {
+						value &= 0xFFFFFFFF;
+
+						this.registers[Register.RC] = ulong.MaxValue;
+					}
+
+					break;
+			}
+
+			switch (parameter.Type) {
 				case ParameterType.Literal: break;
-				case ParameterType.Register: this.registers[p.Register] = value; break;
-				case ParameterType.LiteralAddress: this.memory.WriteU64(p.Literal, value); break;
-				case ParameterType.RegisterAddress: this.memory.WriteU64(this.registers[p.Register], value); break;
-				default: throw new ArgumentException(nameof(p));
+				case ParameterType.Register: this.registers[parameter.Register] = value; break;
+				case ParameterType.LiteralAddress: this.memory.WriteU64(parameter.Literal, value); break;
+				case ParameterType.RegisterAddress: this.memory.WriteU64(this.registers[parameter.Register], value); break;
+				default: throw new ArgumentException(nameof(parameter));
 			}
 		}
 
