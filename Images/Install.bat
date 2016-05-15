2d53355bf7f4f2612a5aa16fd0f0bb97e8fff656
John Brochue <johnbrochue@outlook.com>
2016-05-15 14:03:21 -0400

    Generalized the install script.

--- Images/Install.bat
+++ Images/Install.bat
@@ -1,3 +1,3 @@
 CD %~dp0
-COPY Boot.bin C:\Users\johnb\AppData\Local\Packages\fc7d35ed-c3ad-4862-946c-a21d4fde227c_35t6z52snzrg2\LocalState\Boot.bin
-COPY Program.bin "C:\Users\johnb\AppData\Local\Packages\fc7d35ed-c3ad-4862-946c-a21d4fde227c_35t6z52snzrg2\LocalState\Disk 0.bin"
+COPY Boot.bin "%LOCALAPPDATA%\Packages\fc7d35ed-c3ad-4862-946c-a21d4fde227c_35t6z52snzrg2\LocalState\Boot.bin"
+COPY Program.bin "%LOCALAPPDATA%\Packages\fc7d35ed-c3ad-4862-946c-a21d4fde227c_35t6z52snzrg2\LocalState\Disk 0.bin"
