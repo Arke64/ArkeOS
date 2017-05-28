b6860ea30141734a37f18efb967ec8e5c85ba4cd
John Brochue <johnbrochue@outlook.com>
2017-05-28 18:49:59 -0400

    Remade the projects as .NET standard libraries.

--- ArkeOS.Hardware.Architecture/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyProduct("ArkeOS")]
-[assembly: AssemblyCopyright("Copyright © John Brochue 2016")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
