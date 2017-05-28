c034cd2b4fe7b165ffa0af944abb3043c20f1c9d
John Brochue <johnbrochue@outlook.com>
2017-05-28 19:59:42 -0400

    Added a lot more validation to the install script.

--- Install.bat
+++ Install.bat
@@ -1,11 +1,40 @@
-CD %~dp0
+@ECHO OFF
+
+SETLOCAL
+
+SET ScriptRoot=%~dp0
+
+PUSHD "%ScriptRoot%"
 
 SET Disk=%1
-SET Assembler=.\ArkeOS.Tools.Assembler\bin\Debug\ArkeOS.Tools.Assembler.exe
-SET LocalStorage=%LOCALAPPDATA%\Packages\fc7d35ed-c3ad-4862-946c-a21d4fde227c_kt2p4y5fz58eg\LocalState
+SET PackageId=fc7d35ed-c3ad-4862-946c-a21d4fde227c
+SET Assembler=%ScriptRoot%\ArkeOS.Tools.Assembler\bin\Debug\netcoreapp1.1\ArkeOS.Tools.Assembler.dll
 
-"%Assembler%" ".\Images\Boot.asm"
-"%Assembler%" ".\Images\%Disk%.asm"
+FOR /D %%A IN ("%LOCALAPPDATA%\Packages\%PackageId%*") DO (
+    SET PackageFolder=%%A
+)
 
-COPY ".\Images\Boot.bin" "%LocalStorage%\Boot.bin"
-COPY ".\Images\%Disk%.bin" "%LocalStorage%\Disk 0.bin"
+IF "%PackageFolder" == "" (
+    ECHO Cannot find app pacakge folder.
+    GOTO :EOF
+)
+
+IF "%Disk%" == ""  (
+    ECHO Must provide a disk to run.
+    GOTO :EOF
+)
+
+IF NOT EXIST "%Assembler%" (
+    ECHO Cannot find assembler.
+    GOTO :EOF
+)
+
+dotnet "%Assembler%" "%ScriptRoot%\Images\Boot.asm"
+dotnet "%Assembler%" "%ScriptRoot%\Images\%Disk%.asm"
+
+COPY /Y "%ScriptRoot%\Images\Boot.bin" "%PackageFolder%\LocalState\Boot.bin"
+COPY /Y "%ScriptRoot%\Images\%Disk%.bin" "%PackageFolder%\LocalState\Disk 0.bin"
+
+POPD
+
+ENDLOCAL
