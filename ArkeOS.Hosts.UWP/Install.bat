63609f8418ae53af4786ada25bb2c5904c37f7e5
John Brochue <johnbrochue@outlook.com>
2017-05-29 00:56:47 -0400

    Moved install script to UWP host since it's only for that.

--- ArkeOS.Hosts.UWP/Install.bat
+++ ArkeOS.Hosts.UWP/Install.bat
@@ -8,7 +8,7 @@ PUSHD "%ScriptRoot%"
 
 SET Disk=%1
 SET PackageId=fc7d35ed-c3ad-4862-946c-a21d4fde227c
-SET Assembler=%ScriptRoot%\ArkeOS.Tools.Assembler\bin\Debug\netcoreapp1.1\ArkeOS.Tools.Assembler.dll
+SET Assembler=%ScriptRoot%..\ArkeOS.Tools.Assembler\bin\Debug\netcoreapp1.1\ArkeOS.Tools.Assembler.dll
 
 FOR /D %%A IN ("%LOCALAPPDATA%\Packages\%PackageId%*") DO (
     SET PackageFolder=%%A
@@ -29,11 +29,11 @@ IF NOT EXIST "%Assembler%" (
     GOTO :EOF
 )
 
-dotnet "%Assembler%" "%ScriptRoot%\Images\Boot.asm"
-dotnet "%Assembler%" "%ScriptRoot%\Images\%Disk%.asm"
+dotnet "%Assembler%" "%ScriptRoot%..\Images\Boot.asm"
+dotnet "%Assembler%" "%ScriptRoot%..\Images\%Disk%.asm"
 
-COPY /Y "%ScriptRoot%\Images\Boot.bin" "%PackageFolder%\LocalState\Boot.bin"
-COPY /Y "%ScriptRoot%\Images\%Disk%.bin" "%PackageFolder%\LocalState\Disk 0.bin"
+COPY /Y "%ScriptRoot%..\Images\Boot.bin" "%PackageFolder%\LocalState\Boot.bin"
+COPY /Y "%ScriptRoot%..\Images\%Disk%.bin" "%PackageFolder%\LocalState\Disk 0.bin"
 
 POPD
 
