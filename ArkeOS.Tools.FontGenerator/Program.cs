ba27cb3273e0d2bba42ad8f0a17729fc22d26ed6
John Brochue <johnbrochue@outlook.com>
2017-06-04 15:56:39 -0400

    Dispose of bitmap properly.

--- ArkeOS.Tools.FontGenerator/Program.cs
+++ ArkeOS.Tools.FontGenerator/Program.cs
@@ -16,20 +16,21 @@ namespace ArkeOS.Tools.FontGenerator {
             }
 
             //TODO Remove CoreCompat reference once .NET Standard 2.0 adds System.Drawing
-            var bmp = new Bitmap(args[0]);
-            var final = "";
+            using (var bmp = new Bitmap(args[0])) {
+                var final = "";
 
-            for (var i = 0; i < 95; i++) {
-                var bin = "";
+                for (var i = 0; i < 95; i++) {
+                    var bin = "";
 
-                for (var y = 0; y < 8; y++)
-                    for (var x = 0; x < 5; x++)
-                        bin += bmp.GetPixel(x + i * 5, y).R == 0 ? "1" : "0";
+                    for (var y = 0; y < 8; y++)
+                        for (var x = 0; x < 5; x++)
+                            bin += bmp.GetPixel(x + i * 5, y).R == 0 ? "1" : "0";
 
-                final += "			this.fontData['" + (char)(' ' + i) + "'] = 0x" + Convert.ToUInt64(new string(bin.Reverse().ToArray()), 2).ToString("X") + ";\r\n";
+                    final += "			this.fontData['" + (char)(' ' + i) + "'] = 0x" + Convert.ToUInt64(new string(bin.Reverse().ToArray()), 2).ToString("X") + ";\r\n";
+                }
+
+                Console.Write(final);
             }
-
-            Console.Write(final);
         }
     }
 }
