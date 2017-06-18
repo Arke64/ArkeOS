3fb72c42ed69b5c0502efa5499ed7a819b7ff099
John Brochue <johnbrochue@outlook.com>
2017-06-18 00:53:50 -0400

    Added a file info abstraction to the lexer and calculated line info during peek.

--- ArkeOS.Tools.KohlCompiler/Lexer.cs
+++ ArkeOS.Tools.KohlCompiler/Lexer.cs
@@ -9,57 +9,72 @@ namespace ArkeOS.Tools.KohlCompiler {
     public class Lexer {
         private static IReadOnlyDictionary<int, char[]> ValidDigitsForBase { get; } = new Dictionary<int, char[]> { [2] = new[] { '0', '1' }, [8] = new[] { '0', '1', '2', '3', '4', '5', '6', '7' }, [10] = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }, [16] = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'A', 'B', 'C', 'D', 'E', 'F' } };
 
-        private readonly IReadOnlyList<string> filePaths;
-        private readonly IReadOnlyList<string> fileContents;
-        private readonly int[] fileLengths;
-        private string currentFileContents;
-        private int currentFileLength;
-        private int fileIndex;
-        private int contentsIndex;
+        private class FileInfo {
+            private readonly string contents;
+            private readonly int length;
+            private int index;
+            private int nextNewLine;
+
+            public string Path { get; }
+            public int Line { get; private set; }
+            public int Column { get; private set; }
+            public char Current { get; private set; }
+
+            public FileInfo(string path) {
+                this.contents = File.ReadAllText(path);
+                this.length = this.contents.Length;
+                this.index = 0;
+                this.GetNextNewLine();
+
+                this.Path = path;
+                this.Line = 1;
+                this.Column = 1;
+                this.Current = this.contents[0];
+            }
+
+            public bool Advance() {
+                if (++this.index >= this.length)
+                    return true;
+
+                this.Current = this.contents[this.index];
+                this.Column++;
+
+                if (this.index == this.nextNewLine) {
+                    this.Column = 1;
+                    this.Line++;
+                    this.GetNextNewLine();
+                }
+
+                return false;
+            }
+
+            private void GetNextNewLine() { this.nextNewLine = this.contents.IndexOf(Environment.NewLine, this.nextNewLine, StringComparison.Ordinal); if (this.nextNewLine == -1) this.nextNewLine += Environment.NewLine.Length; }
+        }
+
+        private readonly Queue<FileInfo> files;
+        private FileInfo file;
         private Token currentToken;
 
         public bool AtEnd { get; private set; }
-        public PositionInfo CurrentPosition => this.GetCurrentPositionInfo();
+        public PositionInfo CurrentPosition { get; private set; }
 
         public Lexer(IReadOnlyList<string> filePaths) {
-            this.filePaths = filePaths;
-            this.fileContents = this.filePaths.Select(f => File.ReadAllText(f)).ToList();
-            this.fileLengths = this.fileContents.Select(s => s.Length).ToArray();
-            this.currentFileContents = this.fileContents[0];
-            this.currentFileLength = this.fileLengths[0];
-            this.fileIndex = 0;
-            this.contentsIndex = 0;
+            this.files = new Queue<FileInfo>(filePaths.Select(f => new FileInfo(f)));
+            this.file = this.files.Dequeue();
             this.currentToken = this.LexNextToken();
 
             this.AtEnd = false;
         }
 
         private void Advance() {
-            if (++this.contentsIndex >= this.currentFileLength) {
-                this.contentsIndex = 0;
-
-                if (++this.fileIndex < this.fileContents.Count) {
-                    this.currentFileContents = this.fileContents[this.fileIndex];
-                    this.currentFileLength = this.fileLengths[this.fileIndex];
-                }
-                else {
-                    this.currentFileContents = string.Empty;
-                    this.currentFileLength = 0;
-                }
-            }
+            if (this.file.Advance())
+                this.file = this.files.Any() ? this.files.Dequeue() : null;
         }
 
         private bool PeekChar(out char value) {
-            if (this.contentsIndex < this.currentFileLength) {
-                value = this.currentFileContents[this.contentsIndex];
+            value = this.file?.Current ?? default(char);
 
-                return true;
-            }
-            else {
-                value = default(char);
-
-                return false;
-            }
+            return this.file != null;
         }
 
         private Token ReadIdentifier() {
@@ -133,7 +148,7 @@ namespace ArkeOS.Tools.KohlCompiler {
                 case ')': res = new Token(TokenType.CloseParenthesis, c); break;
                 case '=': res = new Token(TokenType.EqualsSign, c); break;
                 case ';': res = new Token(TokenType.Semicolon, c); break;
-                default: throw new UnexpectedCharacterException(this.GetCurrentPositionInfo(), c);
+                default: throw new UnexpectedCharacterException(this.CurrentPosition, c);
             }
 
             this.Advance();
@@ -147,6 +162,8 @@ namespace ArkeOS.Tools.KohlCompiler {
 
                 return default(Token);
             }
+
+            this.CurrentPosition = new PositionInfo(this.file.Path, this.file.Line, this.file.Column);
 
             if (char.IsLetter(c)) {
                 return this.ReadIdentifier();
@@ -162,17 +179,6 @@ namespace ArkeOS.Tools.KohlCompiler {
             else {
                 return this.ReadSymbol(c);
             }
-        }
-
-        private PositionInfo GetCurrentPositionInfo() {
-            var lengths = this.currentFileContents.Split(new[] { Environment.NewLine }, StringSplitOptions.None).Select(l => l.Length).ToArray();
-            var idx = 0;
-            var ln = 0;
-
-            while (idx + lengths[ln] < this.contentsIndex)
-                idx += lengths[ln++] + Environment.NewLine.Length;
-
-            return new PositionInfo(this.filePaths[this.fileIndex], ln + 1, this.contentsIndex - idx + 1);
         }
 
         public List<Token> ToList() {
