37798e23e9de353e69f9f8a4ab81bfb2da69d8eb
John Brochue <johnbrochue@outlook.com>
2017-06-20 20:58:45 -0400

    Removed an unused exception.

--- ArkeOS.Tools.KohlCompiler/Exceptions/ExpectedCharacterException.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ArkeOS.Tools.KohlCompiler.Exceptions {
-    public class ExpectedCharacterException : Exception {
-        public ExpectedCharacterException(PositionInfo position) : base($"Unexpected end of file in '{position.File}' at {position.Line:N0}:{position.Column:N0}.") { }
-        public ExpectedCharacterException(PositionInfo position, char expected) : base($"Expected character in '{position.File}' at {position.Line:N0}:{position.Column:N0}: '{expected}'.") { }
-    }
-}
